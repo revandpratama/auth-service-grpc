fc93369d0b1b96a194906298701bcf4d70fdfef3
revandpratama <revandaustin@gmail.com>
2024-11-23 13:57:00 +0700

    feat: add user creation, add email and username uniquness checker

--- internal/repository/auth_repo.go
+++ internal/repository/auth_repo.go
@@ -13,6 +13,9 @@ type authRepository struct {
 
 type AuthRepository interface {
 	GetUserByUsername(context context.Context, username string) (*entity.User, error)
+	CreateUser(context context.Context, newUser *entity.User) error
+	IsEmailExists(context context.Context, email string) bool 
+	IsUsernameExists(context context.Context, username string) bool
 }
 
 func NewAuthRepository(db *gorm.DB) AuthRepository {
@@ -27,4 +30,23 @@ func (r *authRepository) GetUserByUsername(context context.Context, username str
 	err := r.db.WithContext(context).First(&user, "username = ?", username).Error
 
 	return &user, err
+}
+
+func (r *authRepository) CreateUser(context context.Context, newUser *entity.User) error {
+	return r.db.WithContext(context).Create(&newUser).Error
+}
+
+func (r *authRepository) IsEmailExists(context context.Context, email string) bool {
+	var user []entity.User
+
+	r.db.WithContext(context).Find(&user, "email = ?", email)
+	
+	return len(user) > 0
+}
+func (r *authRepository) IsUsernameExists(context context.Context, username string) bool {
+	var user []entity.User
+
+	r.db.WithContext(context).Find(&user, "username = ?", username)
+	
+	return len(user) > 0
 }
