b878bcfc5dbd4697ed608ce94990712a8749e44a
revandpratama <revandaustin@gmail.com>
2024-11-23 13:07:45 +0700

    feat: add grpc server adapter

---
+++ adapter/grpc.go
@@ -0,0 +1,49 @@
+package adapter
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/revandpratama/reflect/auth-service/internal/controller"
+	"github.com/revandpratama/reflect/auth-service/pkg/logger"
+	pb "github.com/revandpratama/reflect/auth-service/proto/generated/auth"
+	"google.golang.org/grpc"
+)
+
+type GRPCServer struct {
+	pb.UnimplementedAuthServiceServer
+}
+
+func StartGRPCServer(srv *controller.AuthController) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", ENV.GRPCServerPort))
+	if err != nil {
+		logger.MakeLog(logger.Logger{
+			Level:   logger.LEVEL_FATAL,
+			Message: fmt.Sprintf("Failed to listen: %v", err),
+		})
+	}
+	log.Println("2")
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterAuthServiceServer(grpcServer, srv)
+	log.Println("3")
+
+	logger.MakeLog(logger.Logger{
+		Level:   logger.LEVEL_INFO,
+		Message: "grpc server connected...",
+	})
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			logger.MakeLog(logger.Logger{
+				Level:   logger.LEVEL_FATAL,
+				Message: fmt.Sprintf("Failed to serve: %v", err),
+			})
+		}
+	}()
+
+	logger.MakeLog(logger.Logger{
+		Level:   logger.LEVEL_INFO,
+		Message: "grpc server connected...",
+	})
+}
