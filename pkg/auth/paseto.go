db2d00452f2d57bcaa47cde8640509464219f959
revandpratama <revandaustin@gmail.com>
2024-11-24 14:40:09 +0700

    chore: remove unused commented code

--- pkg/auth/paseto.go
+++ pkg/auth/paseto.go
@@ -15,7 +15,7 @@ func CreateToken(user *entity.User) (string, error) {
 	token := paseto.NewToken()
 
 	//set rule
-	token.SetIssuedAt(time.Now()) // Set the `iat` claim
+	token.SetIssuedAt(time.Now()) 
 	token.SetNotBefore(time.Now())
 	token.SetExpiration(time.Now().Add(2 * time.Minute))
 
@@ -26,9 +26,7 @@ func CreateToken(user *entity.User) (string, error) {
 	token.SetString("username", user.Username)
 	token.SetString("email", user.Email)
 
-	// Private key (DO NOT SHARE)
 
-	// signed := token.V4Sign(secretKey, nil)
 	encrypted := token.V4Encrypt(secretKey, nil)
 
 	var err error
@@ -37,10 +35,7 @@ func CreateToken(user *entity.User) (string, error) {
 	}
 
 	return encrypted, err
-	// return &Token{
-	// 	publicKeyHex: publicKey.ExportHex(),
-	// 	signedToken:  signed,
-	// }, err
+
 }
 
 func VerifyToken(encryptedToken string) (*entity.User, error) {
@@ -49,20 +44,12 @@ func VerifyToken(encryptedToken string) (*entity.User, error) {
 	parser.AddRule(paseto.NotExpired())
 	parser.AddRule(paseto.ValidAt(time.Now()))
 
-	// publicKey, err := paseto.NewV4AsymmetricPublicKeyFromHex(publicKeyHex)
-	// if err != nil {
-	// 	return nil, err
-	// }
+
 
 	parsedToken, err := parser.ParseV4Local(secretKey, encryptedToken, nil)
 	if err != nil {
 		return nil, err
 	}
-	// Parse and validate the token
-	// parsedToken, err := parser.ParseV4Public(publicKey, signedToken, nil)
-	// if err != nil {
-	// 	return nil, err
-	// }
 
 	user, err := getPayloadFromParsedToken(parsedToken)
 
@@ -111,36 +98,3 @@ func getPayloadFromParsedToken(parsedToken *paseto.Token) (*entity.User, error) 
 	return &user, nil
 }
 
-// func AuthTest() {
-
-// 	user := entity.User{
-// 		ID:       uuid.New(),
-// 		Name:     "Black Star",
-// 		Username: "black.star",
-// 		Email:    "black@gmail.com",
-// 	}
-
-// 	signedToken, err := CreateToken(&user)
-// 	if err != nil {
-// 		log.Println(err)
-// 		return
-// 	}
-// 	fmt.Println("signedToken :", signedToken)
-
-// 	parsedToken, err := VerifyToken(signedToken)
-// 	if err != nil {
-// 		log.Println(err)
-// 		return
-// 	}
-
-// 	fmt.Println("parsedToken : ", parsedToken)
-
-// 	userID, err := parsedToken.GetString("user_id")
-// 	name, err := parsedToken.GetString("name")
-// 	username, err := parsedToken.GetString("username")
-
-// 	fmt.Println(userID)
-// 	fmt.Println(name)
-// 	fmt.Println(username)
-
-// }
