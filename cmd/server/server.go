235aef35cdd560c88ab8fe33f8d1d21ff1331ba2
revandpratama <revandaustin@gmail.com>
2024-11-24 22:06:56 +0700

    refactor: implement graceful shutdown and add cleanup function

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,24 +1,34 @@
 package server
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/revandpratama/reflect/auth-service/adapter"
 	"github.com/revandpratama/reflect/auth-service/client"
 	"github.com/revandpratama/reflect/auth-service/internal/controller"
 	"github.com/revandpratama/reflect/auth-service/internal/repository"
 	"github.com/revandpratama/reflect/auth-service/internal/service"
+	"github.com/revandpratama/reflect/auth-service/pkg/logger"
 )
 
 type Server struct {
+	shutdown chan os.Signal
+	done     chan bool
 }
 
 func NewServer() *Server {
-	return &Server{}
+	return &Server{
+		shutdown: make(chan os.Signal, 1),
+		done:     make(chan bool, 1),
+	}
 }
 
 func (s *Server) Start() {
-
+	signal.Notify(s.shutdown, os.Interrupt, syscall.SIGTERM)
 	//initialize app
 	adapter.LoadConfig()
 	adapter.ConnectDB()
@@ -26,9 +36,67 @@ func (s *Server) Start() {
 	authRepository := repository.NewAuthRepository(adapter.DB)
 	authService := service.NewAuthService(authRepository)
 	authController := controller.NewAuthController(authService)
-	
-	
-	adapter.StartGRPCServer(authController)
-	
+
+	go func() {
+		logger.MakeLog(logger.Logger{
+			Level:   logger.LEVEL_INFO,
+			Message: "starting grpc server...",
+		})
+
+		if err := adapter.StartGRPCServer(authController); err != nil {
+			log.Printf("Error starting gRPC server: %v", err)
+			s.done <- true
+		}
+	}()
+
 	client.TestClient()
+
+	select {
+	case sig := <-s.shutdown:
+		logger.MakeLog(logger.Logger{
+			Level:   logger.LEVEL_INFO,
+			Message: fmt.Sprintf("shutting down... :%v", sig),
+		})
+	case <-s.done:
+		logger.MakeLog(logger.Logger{
+			Level:   logger.LEVEL_FATAL,
+			Message: "server stopped due to error",
+		})
+	}
+
+	s.cleanup()
+
+}
+
+func (s *Server) cleanup() {
+	// Add cleanup logic here
+	logger.MakeLog(logger.Logger{
+		Level:   logger.LEVEL_WARN,
+		Message: "cleaning up resources...",
+	})
+
+	sqlDB, err := adapter.DB.DB()
+	if err != nil {
+		logger.MakeLog(logger.Logger{
+			Level:   logger.LEVEL_ERROR,
+			Message: err.Error(),
+		})
+	}
+	// Close database connection
+	if sqlDB != nil {
+		if err := sqlDB.Close(); err != nil {
+			logger.MakeLog(logger.Logger{
+				Level:   logger.LEVEL_ERROR,
+				Message: fmt.Sprintf("Error closing database connection: %v", err),
+			})
+
+		}
+	}
+
+	// Add any other cleanup tasks here
+
+	logger.MakeLog(logger.Logger{
+		Level:   logger.LEVEL_INFO,
+		Message: "server shutdown complete",
+	})
 }
