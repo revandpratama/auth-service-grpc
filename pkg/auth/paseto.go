6194d16591116396d608773dae23eb72a5754ba4
revandpratama <revandaustin@gmail.com>
2024-11-24 14:31:41 +0700

    refactor: change how verifytoken handle token and return user entity instead of a parsedToken

--- pkg/auth/paseto.go
+++ pkg/auth/paseto.go
@@ -21,6 +21,7 @@ func CreateToken(user *entity.User) (string, error) {
 
 	//insert paylaod
 	token.SetString("user_id", strconv.Itoa(user.ID))
+	token.SetString("role_id", strconv.Itoa(user.RoleID))
 	token.SetString("name", user.Name)
 	token.SetString("username", user.Username)
 	token.SetString("email", user.Email)
@@ -42,7 +43,7 @@ func CreateToken(user *entity.User) (string, error) {
 	// }, err
 }
 
-func VerifyToken(encryptedToken string) (*paseto.Token, error) {
+func VerifyToken(encryptedToken string) (*entity.User, error) {
 
 	parser := paseto.NewParser()
 	parser.AddRule(paseto.NotExpired())
@@ -63,7 +64,51 @@ func VerifyToken(encryptedToken string) (*paseto.Token, error) {
 	// 	return nil, err
 	// }
 
-	return parsedToken, err
+	user, err := getPayloadFromParsedToken(parsedToken)
+
+	return user, err
+}
+
+func getPayloadFromParsedToken(parsedToken *paseto.Token) (*entity.User, error) {
+	userIDStr, err := parsedToken.GetString("user_id")
+	if err != nil {
+		return nil, err
+	}
+	roleIDStr, err := parsedToken.GetString("role_id")
+	if err != nil {
+		return nil, err
+	}
+	name, err := parsedToken.GetString("name")
+	if err != nil {
+		return nil, err
+	}
+	username, err := parsedToken.GetString("username")
+	if err != nil {
+		return nil, err
+	}
+	email, err := parsedToken.GetString("email")
+	if err != nil {
+		return nil, err
+	}
+
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		return nil, err
+	}
+	roleID, err := strconv.Atoi(roleIDStr)
+	if err != nil {
+		return nil, err
+	}
+
+	user := entity.User{
+		ID:       userID,
+		RoleID:   roleID,
+		Name:     name,
+		Email:    email,
+		Username: username,
+	}
+
+	return &user, nil
 }
 
 // func AuthTest() {
