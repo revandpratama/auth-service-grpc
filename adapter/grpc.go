12f96a2348317344d0874d979d8cc572e41744b1
revandpratama <revandaustin@gmail.com>
2024-12-24 23:50:45 +0700

    REFACTOR: refactor the whole grpc adapter

--- adapter/grpc.go
+++ adapter/grpc.go
@@ -1,50 +1,65 @@
 package adapter
 
 import (
-	"fmt"
-	"net"
-
-	"github.com/revandpratama/reflect/auth-service/internal/controller"
-	"github.com/revandpratama/reflect/auth-service/pkg/logger"
-	pb "github.com/revandpratama/reflect/auth-service/proto/generated/auth"
 	"google.golang.org/grpc"
 )
 
-type GRPCServer struct {
-	pb.UnimplementedAuthServiceServer
+func GRPC() Option {
+	return &grpcserver{}
 }
 
-func StartGRPCServer(srv *controller.AuthController) error {
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", ENV.GRPCServerPort))
-	if err != nil {
-		logger.MakeLog(logger.Logger{
-			Level:   logger.LEVEL_FATAL,
-			Message: fmt.Sprintf("Failed to listen: %v", err),
-		})
-		return err
-	}
+type grpcserver struct {
+	adapter *Adapter
+}
+
+func (g *grpcserver) Start(a *Adapter) error {
 
 	grpcServer := grpc.NewServer()
 
-	pb.RegisterAuthServiceServer(grpcServer, srv)
-
-	logger.MakeLog(logger.Logger{
-		Level:   logger.LEVEL_INFO,
-		Message: "grpc server connected...",
-	})
-
-	if err := grpcServer.Serve(listener); err != nil {
-		logger.MakeLog(logger.Logger{
-			Level:   logger.LEVEL_FATAL,
-			Message: fmt.Sprintf("Failed to serve: %v", err),
-		})
-		return err
-	}
-
-	logger.MakeLog(logger.Logger{
-		Level:   logger.LEVEL_INFO,
-		Message: "grpc server connected...",
-	})
+	a.GRPCServer = grpcServer
+	g.adapter = a
 
 	return nil
 }
+
+func (g *grpcserver) Stop() error {
+
+	Adapters.GRPCServer.Stop()
+
+	return nil
+}
+
+// func StartGRPCServer(srv *controller.AuthController) error {
+// 	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", ENV.GRPCServerPort))
+// 	if err != nil {
+// 		// logger.MakeLog(logger.Logger{
+// 		// 	Level:   logger.LEVEL_FATAL,
+// 		// 	Message: fmt.Sprintf("Failed to listen: %v", err),
+// 		// })
+// 		return err
+// 	}
+
+// 	grpcServer := grpc.NewServer()
+
+// 	pb.RegisterAuthServiceServer(grpcServer, srv)
+
+// 	// logger.MakeLog(logger.Logger{
+// 	// 	Level:   logger.LEVEL_INFO,
+// 	// 	Message: "grpc server connected...",
+// 	// })
+
+// 	if err := grpcServer.Serve(listener); err != nil {
+// 		// logger.MakeLog(logger.Logger{
+// 		// 	Level:   logger.LEVEL_FATAL,
+// 		// 	Message: fmt.Sprintf("Failed to serve: %v", err),
+// 		// })
+// 		return err
+// 	}
+
+// 	// logger.MakeLog(logger.Logger{
+// 	// 	Level:   logger.LEVEL_INFO,
+// 	// 	Message: "grpc server connected...",
+// 	// })
+
+// 	return nil
+// }
