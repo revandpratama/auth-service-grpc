a7cce98f581a145cd777d1338b047c24e6b9676f
revandpratama <revandaustin@gmail.com>
2024-12-25 00:24:28 +0700

    FEAT: move whole config to its own package

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
+
+	GRPCServerPort string
+
+	KafkaHost     string
+	KafkaPort     string
+	KafkaClientID string
+}
+
+var ENV *Config
+
+func LoadConfig() error {
+
+	viper.AddConfigPath(".")
+	viper.SetConfigType("env")
+	viper.SetConfigName(".env")
+
+	if err := viper.ReadInConfig(); err != nil {
+
+		return err
+	}
+
+	if err := viper.Unmarshal(&ENV); err != nil {
+		return err
+	}
+
+	return nil
+}
